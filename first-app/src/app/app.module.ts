91bc23f4214c585cea7e71e9ba889040b9883bbf
yoon jun Y <mjhn010@naver.com>
2023-07-31 18:27:31 +0900

    component주입하기

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
-@NgModule({
+@NgModule({//모든 컴포넌트를 알지못하기에 여기에 등록해서 알려줘야한다.
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule
