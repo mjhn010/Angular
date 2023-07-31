7d5a55416af857e322e38133719d7ef5e2658f90
yoon jun Y <mjhn010@naver.com>
2023-07-31 17:51:11 +0900

    serverComponentAdd

---
+++ first-app/src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'app-server',//접두사 사용하기 app-폴더명
+  templateUrl:'./server.component.html',//경로지정
+  // styleUrls: style.css지정
+
+})
+export class ServerComponent{
+
+}
