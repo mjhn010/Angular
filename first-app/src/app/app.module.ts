272d591c350d12f16cd006c0dda1e1e13fcff96b
yoon jun Y <mjhn010@naver.com>
2023-08-01 18:15:41 +0900

    폴더구조

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({//모든 컴포넌트를 알지못하기에 여기에 등록해서 알려줘야한다.
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule
