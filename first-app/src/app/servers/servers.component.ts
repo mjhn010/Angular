ad9e21f76f02ad2442ab0e6c027b084a3d1ad411
yoon jun Y <mjhn010@naver.com>
2023-08-02 18:48:03 +0900

    event

--- first-app/src/app/servers/servers.component.ts
+++ first-app/src/app/servers/servers.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
  allowNewServer:boolean =true;
+ serverCreationStatus:String = 'No server was created !'
 
   constructor(){
     // setTimeout(function(){//seTimeout이 실행되면서 this는
@@ -21,6 +22,9 @@ export class ServersComponent implements OnInit {
 
   ngOnInit() {
 
+ }
+ onCreateServer(){
+  this.serverCreationStatus = 'server was created'
  }
 
 }
